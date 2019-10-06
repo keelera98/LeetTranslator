c56f87c76036a870ac147bb758d65e5488f926bc
Austin A. Keeler <aak07504@lincoln.sjfc.edu>
2019-10-06 16:52:25 -0400

    Add Command Line Arguments

--- Homework03.c
+++ Homework03.c
@@ -1,92 +1,71 @@
 #include <stdio.h>
 #include <malloc.h>
-include <string.h>
+#include <string.h>
 
-int main(){
+int main(int argc, char** argv){
 	char read;
 	FILE* out;
 	FILE* fp;
-	char* fileInput;
-	char* fileOutput;
 	char* str;
-	int pos = 0;
 
-	str = malloc(sizeof(char) * 3);
+	str = malloc(sizeof(char) * 4);
 
-	if((fileInput = malloc(sizeof(char) * 100)) && (fileOutput = malloc(sizeof(char) * 100))){
-		printf("Enter a file name: ");
-		fgets(fileInput, 99, stdin);
+	if((fp = fopen(argv[1], "r")) && (out = fopen(argv[2], "w"))){
+		while((read = fgetc(fp)) != EOF){
 		
-		strtok(fileInput, "\n");
-
-		printf("Enter an output file name: ");
-		fgets(fileOutput, 99, stdin);
-
-		strtok(fileOutput, "\n");
-
-		if((fp = fopen(fileInput, "r")) && (out = fopen(fileOutput, "w"))){
-			while((read = fgetc(fp)) != EOF){
-		
-				switch(read){
-					case 'A':
-					case 'a':	
-						fputc('4', out);
-						break;
-					case 'C':
-					case 'c':
-						fputc('(', out);
-						break;
-					case 'E':
-					case 'e':
-						fputc('3', out);
-						break;
-					case 'F':
-					case 'f':
-						fputs("ph", out);
-						break;
-					case 'I':
-					case 'i':	
-						fputc('1', out);
-						break;
-					case 'G':
-					case 'g':
-						fputc('6', out);
-						break;
-					case 'O':
-					case 'o':
-						fputc('0', out);
-						break;
-					case 'S':
-					case 's':
-						fputc('5', out);
-						break;
-					case 'T':
-					case 't':
-						fputc('7', out);
-						break;
-					case 'Y':
-					case 'y':
-						pos = ftell(fp);
-						fgets(str, 2, fp);
-						if(!(strcmp(str, "ou")))
-							fputc('U', out);
-						else{
-							fseek(fp, pos, SEEK_SET);
-						}
-						break;
-					default:
-						fputc(read, out);
-				}
+			switch(read){
+				case 'A':
+				case 'a':	
+					fputc('4', out);
+					break;
+				case 'C':
+				case 'c':
+					fputc('(', out);
+					break;
+				case 'E':
+				case 'e':
+					fputc('3', out);
+					break;
+				case 'F':
+				case 'f':
+					fputs("ph", out);
+					break;
+				case 'I':
+				case 'i':	
+					fputc('1', out);
+					break;
+				case 'G':
+				case 'g':
+					fputc('6', out);
+					break;
+				case 'O':
+				case 'o':
+					fputc('0', out);
+					break;
+				case 'S':
+				case 's':
+					fputc('5', out);
+					break;
+				case 'T':
+				case 't':
+					fputc('7', out);
+					break;
+				case 'Y':
+				case 'y':
+					fgets(str, 3, fp);
+					if(!(strcmp(str, "ou")))
+						fputc('U', out);
+					else{
+						fputs("y0", out);
+					}
+					break;
+				default:
+					fputc(read, out);
 			}
-			fclose(fp);
-			fclose(out);
-		}else{
-			perror("Error");
 		}
+		fclose(fp);
+		fclose(out);
 	}
 	printf("TRANSLATION COMPLETE\n");
-	free(fileInput);
-	free(fileOutput);
 	free(str);
 }
-
